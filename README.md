c194114e55eafd3d4f6b31daa0bf148083041a47
Paulo Gorris <paulo.gorris@gmail.com>
2021-04-11 10:05:29 -0300

    apply smooth scrolling and lightbox gallery

--- README.md
+++ README.md
@@ -2,15 +2,15 @@
 
 ## General Info
 
-This is a project of a small local construction company. It's a simple landing page.
+This is a project of a small local construction company. It's a simple landing page. The real state photos were provided by the client.
 
 ### Technologies
 
 - HTML5
 - CSS3
-- Bootstrap 5
-- Sass
 - Javascript
+- jQuery
+- Lightbox2
 
 ### Launch
 
--- js/app.js
+++ js/app.js
@@ -21,3 +21,23 @@ const navSlide = () => {
 };
 
 navSlide();
+
+// Smooth Scrolling
+$("#navbar a, .btn").on("click", function (e) {
+  if (this.hash !== "") {
+    e.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top - 100,
+      },
+      800
+    );
+  }
+});
+
+lightbox.option({
+  albumLabel: "",
+});
